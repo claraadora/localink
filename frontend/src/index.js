08504daa5ae126d43c6ded87076cbb39b0ebfc07
Clara Adora <claraadora@gmail.com>
2020-05-18 03:39:45 +0800

    Create store provider for App.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,13 @@ import { createStore } from "redux";
 import App from "./App";
 import { theme } from "./theme";
 import { ThemeProvider } from "@material-ui/core/styles";
-
-// const store = createStore(rootReducer);
+import store from "./store";
 
 render(
-  <ThemeProvider theme={theme}>
-    <App />,
-  </ThemeProvider>,
-  document.getElementById("root")
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />,
+    </ThemeProvider>
+    , document.getElementById("root")
+  </Provider>
 );
