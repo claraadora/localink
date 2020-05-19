092db8c28d420fa855d4b4f48168416c20250792
Clara Adora <claraadora@gmail.com>
2020-05-19 22:55:59 +0800

    Fix theme

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,6 +1,7 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import lightGreen from "@material-ui/core/colors/lightGreen";
 import yellow from "@material-ui/core/colors/yellow";
+import grey from "@material-ui/core/colors/grey";
 
 export const theme = createMuiTheme({
   palette: {
@@ -12,8 +13,40 @@ export const theme = createMuiTheme({
     },
   },
   typography: {
+    htmlFontSize: 16,
+    fontFamily: '"Metropolis", sans-serif',
+    fontSize: 13,
+    fontWeight: 400,
+    h1: {
+      fontFamily: '"Metropolis", sans-serif',
+      fontWeight: 600,
+      fontSize: "2.125rem",
+    },
+    h6: {
+      fontFamily: '"Metropolis", sans-serif',
+      fontWeight: 500,
+      fontSize: "0.9375rem",
+    },
+    subtitle1: {
+      fontFamily: '"Metropolis", sans-serif',
+      fontWeight: "normal",
+      fontSize: "0.6875rem",
+    },
+    body1: {
+      fontFamily: '"Metropolis", sans-serif',
+      fontWeight: "normal",
+      fontSize: "0.8125rem",
+      lineHeight: 1.38,
+      letterSpacing: "normal",
+      color: grey[900],
+    },
     button: {
+      fontFamily: '"Metropolis", sans-serif',
+      fontWeight: 500,
+      fontSize: "1.3rem",
+      letterSpacing: 1,
       textTransform: "none",
+      color: grey[100],
     },
   },
 });
