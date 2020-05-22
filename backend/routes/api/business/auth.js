8887a95501a9484e16bc8f3bff78fe99058d76f7
olivia johansen <oliviajjohansen@gmail.com>
2020-05-23 05:12:39 +0800

    set up login, signup, loaduser for business owners

---
+++ backend/routes/api/business/auth.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const auth = require('../../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const Business = require('../../../models/Business');
+
+// @route    GET business/auth
+// @desc     Get user by token (load user for frontend)
+// @access   Private
+// @return   User token
+router.get('/', auth, async (req, res) => {
+  try {
+    const business = await Business.findById(req.user.id);
+    res.json(business);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    POST business/auth
+// @desc     Authenticate user & get token (login)
+// @access   Public
+// @return   User token
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let business = await Business.findOne({ email });
+
+      if (!business) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, business.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        business: {
+          id: business.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '5 days' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('API running'));
 
+//Define routes
+app.use('/business', require('./routes/api/business/index'));
+app.use('/business/auth', require('./routes/api/business/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
