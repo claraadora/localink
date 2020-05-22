79b5e5809b08586dfd71e2f8a949694dda85b948
olivia johansen <oliviajjohansen@gmail.com>
2020-05-23 04:14:58 +0800

    set up middleware and models

---
+++ backend/models/Business.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const BusinessSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  shopName: {
+    type: String,
+    required: true
+  },
+  shop: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Shop'
+  }
+});
+
+module.exports = Business = mongoose.model('Business', BusinessSchema);
