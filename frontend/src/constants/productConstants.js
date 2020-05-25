72c58da4f9e35dc548d89706ec0442ef913e1fec
Clara Adora <claraadora@gmail.com>
2020-05-25 16:57:13 +0800

    Add action creators for products

---
+++ frontend/src/constants/productConstants.js
@@ -0,0 +1,7 @@
+const productConstants = {
+  GET_PRODUCT_LIST: "GET_PRODUCT_LIST",
+  UPDATE_PRODUCT_LIST: "UPDATE_PRODUCT_LIST",
+  PRODUCT_ERROR: "PRODUCT_ERROR",
+};
+
+export default productConstants;
