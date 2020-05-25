608f08b19ac909b1cb1d7d9e4a2b8c72e4e2cfd4
olivia johansen <oliviajjohansen@gmail.com>
2020-05-25 11:39:38 +0800

    change return type from updated shop to updated or new product

--- backend/routes/api/business/profile.js
+++ backend/routes/api/business/profile.js
@@ -100,10 +100,9 @@ router.post(
   [
     auth,
     check('email', 'Please include a valid email').isEmail(),
-    check(
-      'oldPassword',
-      'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 }),
+    check('oldPassword', 'Please enter your existing password').isLength({
+      min: 6
+    }),
     check(
       'newPassword',
       'Please enter a password with 6 or more characters'
