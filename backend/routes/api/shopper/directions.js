da82b5aecfa61285fa6520ffab3701daea8bc946
olivia johansen <oliviajjohansen@gmail.com>
2020-05-27 18:29:05 +0800

    display alternative routes of distinct colors

---
+++ backend/routes/api/shopper/directions.js
@@ -0,0 +1,9 @@
+const initURI = 'https://www.google.com/maps/dir/?api=1';
+const apiKey = 'AIzaSyBjucyjCeaC5ddig7Hd_RyzPrqWiBIwXhM';
+
+const currentLocation = 'origin="';
+const destinations = [];
+
+function toURL(location) {
+  const arr = location.split(' ');
+}
