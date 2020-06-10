d6f97fc7db13c4d9ab9cdf1a8efc71451c41e690
Clara Adora <claraadora@gmail.com>
2020-06-11 00:52:13 +0800

    Add autosizer for window

--- frontend/src/components/window/Window.js
+++ frontend/src/components/window/Window.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { FixedSizeList as List } from "react-window";
-
+import AutoSizer from "react-virtualized-auto-sizer";
 import { LocationCard } from "../card/LocationCard";
+import "./styles.css";
 
-const Row = ({ index }) => <div>Row {index}</div>;
+const Row = ({ index, style }) => <div style={style}>Row {index}</div>;
 
 export const LocationList = () => (
-  <List height={400} itemCount={1000} itemSize={35} width="100%">
-    {Row}
-  </List>
+  <AutoSizer>
+    {({ height, width }) => (
+      <List
+        className="List"
+        height={height}
+        itemCount={1000}
+        itemSize={35}
+        width={width}
+      >
+        {Row}
+      </List>
+    )}
+  </AutoSizer>
 );
