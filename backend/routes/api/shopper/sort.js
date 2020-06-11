669600459639fb839ff8243a47cf7cb05dd6b4b5
olivia johansen <oliviajjohansen@gmail.com>
2020-06-11 20:36:39 +0800

    Dynamic sort function, sort by dist,price.ratings

---
+++ backend/routes/api/shopper/sort.js
@@ -0,0 +1,262 @@
+const arr = [
+  {
+    name: 'gaming game for gamers',
+    image:
+      'https://sg-test-11.slatic.net/p/c421749b614332a5d647f9cafbdcf1a5.jpg',
+    description: 'For gamers',
+    price: 99.99,
+    shop_docs: [
+      {
+        _id: '5ee10c5b9f15a9ec73867288',
+        owner: '5ee10b7fd3dc884052fee119',
+        __v: 10,
+        address: '220 Orchard Rd, Orchard',
+        avatar:
+          'https://www.sephora.com/contentimages/homepage/012120/Homepage/DesktopMweb/2020-01-27-hp-beauty-offer-generic-us-ca-d-slice.jpg?imwidth=482',
+        description:
+          "Explore endless beauty from the world's most desired beauty brands",
+        distance: 11057,
+        latLng: {
+          lat: 1.3017106,
+          lng: 103.838732
+        },
+        shopName: 'bbread',
+        products: [
+          '5ee21a0f5e6cfd6bf5cbdc4d',
+          '5ee110c7d3dc884052fee125',
+          '5ee11075d3dc884052fee124',
+          '5ee11010d3dc884052fee123',
+          '5ee10fd0d3dc884052fee122',
+          '5ee10f81d3dc884052fee121',
+          '5ee10f4ad3dc884052fee11f'
+        ],
+        reviews: [
+          '5ee111a5d3dc884052fee129',
+          '5ee11148d3dc884052fee128',
+          '5ee11128d3dc884052fee126'
+        ],
+        ratings: 2.9166666666666665
+      }
+    ],
+    score: 1.034369707107544
+  },
+  {
+    name:
+      '(Switch) Overcooked Bundle: The Overcooked! + Overcooked! 2 English Game',
+    image:
+      'https://sg-test-11.slatic.net/p/038b84a47d8b4922c4a2a9ab5bdd656b.jpg',
+    description:
+      'Online/Local Multiplayer Madness - You’ll knead to work together to get the highest score in chaotic local and online multiplayer Feast Your Eyes on This - Cook in new kitchens ranging from sushi restaurants, magic schools and mines Romaine Calm - Travel through teleporters, across moving platforms and save time by throwing ingredients across dynamic kitchens that shift and evolve Whet Your Appetite - Cook up a range of new recipes including sushi, cakes, burgers and pizzas',
+    price: 49.7,
+    shop_docs: [
+      {
+        _id: '5ee10bca9f15a9ec738633ef',
+        owner: '5ee10b4fd3dc884052fee116',
+        __v: 7,
+        address: '629 Aljunied Rd, #04-02A, Singapore 389838',
+        avatar:
+          'https://i.pcmag.com/imagery/articles/05pGSLeBVTWQNoDPNG2midy-4.fit_scale.size_2698x1517.v1573262409.jpg',
+        description:
+          'Welcome I am a super cool gaming shop that sells gaming sets, equipments and accessories to spice up your gaming life',
+        distance: 18115,
+        latLng: {
+          lat: 1.3263764,
+          lng: 103.8795565
+        },
+        shopName: 'abread',
+        products: [
+          '5ee11369d3dc884052fee132',
+          '5ee11333d3dc884052fee131',
+          '5ee1125bd3dc884052fee12d',
+          '5ee11226d3dc884052fee12c',
+          '5ee111f6d3dc884052fee12a'
+        ],
+        reviews: ['5ee112cfd3dc884052fee130', '5ee112abd3dc884052fee12e'],
+        ratings: 3.5
+      }
+    ],
+    score: 0.8406271934509277
+  },
+  {
+    name:
+      '(Switch) Overcooked Bundle: The Overcooked! + Overcooked! 2 English Game',
+    image:
+      'https://sg-test-11.slatic.net/p/038b84a47d8b4922c4a2a9ab5bdd656b.jpg',
+    description:
+      'Online/Local Multiplayer Madness - You’ll knead to work together to get the highest score in chaotic local and online multiplayer Feast Your Eyes on This - Cook in new kitchens ranging from sushi restaurants, magic schools and mines Romaine Calm - Travel through teleporters, across moving platforms and save time by throwing ingredients across dynamic kitchens that shift and evolve Whet Your Appetite - Cook up a range of new recipes including sushi, cakes, burgers and pizzas',
+    price: 49.7,
+    shop_docs: [
+      {
+        _id: '5ee10bca9f15a9ec738633ef',
+        owner: '5ee10b4fd3dc884052fee116',
+        __v: 7,
+        address: '629 Aljunied Rd, #04-02A, Singapore 389838',
+        avatar:
+          'https://i.pcmag.com/imagery/articles/05pGSLeBVTWQNoDPNG2midy-4.fit_scale.size_2698x1517.v1573262409.jpg',
+        description:
+          'Welcome I am a super cool gaming shop that sells gaming sets, equipments and accessories to spice up your gaming life',
+        distance: 18115,
+        latLng: {
+          lat: 1.3263764,
+          lng: 103.8795565
+        },
+        shopName: 'abread',
+        products: [
+          '5ee11369d3dc884052fee132',
+          '5ee11333d3dc884052fee131',
+          '5ee1125bd3dc884052fee12d',
+          '5ee11226d3dc884052fee12c',
+          '5ee111f6d3dc884052fee12a'
+        ],
+        reviews: ['5ee112cfd3dc884052fee130', '5ee112abd3dc884052fee12e'],
+        ratings: 3.5
+      }
+    ],
+    score: 1.0813095569610596
+  },
+  {
+    name:
+      'Sony Singapore PlayStation 4 500GB Slim Console with 2 DUALSHOCK 4 gaming controllers',
+    image:
+      'https://sg-test-11.slatic.net/p/c421749b614332a5d647f9cafbdcf1a5.jpg',
+    description:
+      'Discover all of the amazing connectivity and personalisation features that make PlayStation®4 the destination for the best gaming and entertainment experiences',
+    price: 179.8,
+    shop_docs: [
+      {
+        _id: '5ee10bca9f15a9ec738633ef',
+        owner: '5ee10b4fd3dc884052fee116',
+        __v: 7,
+        address: '629 Aljunied Rd, #04-02A, Singapore 389838',
+        avatar:
+          'https://i.pcmag.com/imagery/articles/05pGSLeBVTWQNoDPNG2midy-4.fit_scale.size_2698x1517.v1573262409.jpg',
+        description:
+          'Welcome I am a super cool gaming shop that sells gaming sets, equipments and accessories to spice up your gaming life',
+        distance: 18115,
+        latLng: {
+          lat: 1.3263764,
+          lng: 103.8795565
+        },
+        shopName: 'abread',
+        products: [
+          '5ee11369d3dc884052fee132',
+          '5ee11333d3dc884052fee131',
+          '5ee1125bd3dc884052fee12d',
+          '5ee11226d3dc884052fee12c',
+          '5ee111f6d3dc884052fee12a'
+        ],
+        reviews: ['5ee112cfd3dc884052fee130', '5ee112abd3dc884052fee12e'],
+        ratings: 3.5
+      }
+    ],
+    score: 1.8081716299057007
+  },
+  {
+    name: '(Ducky One 2 SF White Edition Mechanical Keyboard for gaming',
+    image:
+      'https://sg-test-11.slatic.net/p/6f20804e7a58805e1f36cda450a57eaa.png',
+    description:
+      'Brand:Ducky Model:One 2 SF Size:65% Frame Color:White Primary LED Color:RGB USB Key Rollover:Full Interface(s):USB-C Windows Compatible:Yes Cord Length:60 inches',
+    price: 179.9,
+    shop_docs: [
+      {
+        _id: '5ee10bca9f15a9ec738633ef',
+        owner: '5ee10b4fd3dc884052fee116',
+        __v: 7,
+        address: '629 Aljunied Rd, #04-02A, Singapore 389838',
+        avatar:
+          'https://i.pcmag.com/imagery/articles/05pGSLeBVTWQNoDPNG2midy-4.fit_scale.size_2698x1517.v1573262409.jpg',
+        description:
+          'Welcome I am a super cool gaming shop that sells gaming sets, equipments and accessories to spice up your gaming life',
+        distance: 18115,
+        latLng: {
+          lat: 1.3263764,
+          lng: 103.8795565
+        },
+        shopName: 'abread',
+        products: [
+          '5ee11369d3dc884052fee132',
+          '5ee11333d3dc884052fee131',
+          '5ee1125bd3dc884052fee12d',
+          '5ee11226d3dc884052fee12c',
+          '5ee111f6d3dc884052fee12a'
+        ],
+        reviews: ['5ee112cfd3dc884052fee130', '5ee112abd3dc884052fee12e'],
+        ratings: 3.5
+      }
+    ],
+    score: 0.6184504628181458
+  },
+  {
+    name: 'Razer Gigantus V2 Large Mouse Mat For Gaming (2020 Model)',
+    image:
+      'https://sg-test-11.slatic.net/p/ded053993cbcc8ff08be18ced55b4b57.png',
+    description:
+      'Available in four different sizes: Medium, Large, XXL, 3XL Textured micro-weave cloth surface Thick, high-density rubber foam Anti-slip base',
+    price: 120,
+    shop_docs: [
+      {
+        _id: '5ee10bca9f15a9ec738633ef',
+        owner: '5ee10b4fd3dc884052fee116',
+        __v: 7,
+        address: '629 Aljunied Rd, #04-02A, Singapore 389838',
+        avatar:
+          'https://i.pcmag.com/imagery/articles/05pGSLeBVTWQNoDPNG2midy-4.fit_scale.size_2698x1517.v1573262409.jpg',
+        description:
+          'Welcome I am a super cool gaming shop that sells gaming sets, equipments and accessories to spice up your gaming life',
+        distance: 18115,
+        latLng: {
+          lat: 1.3263764,
+          lng: 103.8795565
+        },
+        shopName: 'abread',
+        products: [
+          '5ee11369d3dc884052fee132',
+          '5ee11333d3dc884052fee131',
+          '5ee1125bd3dc884052fee12d',
+          '5ee11226d3dc884052fee12c',
+          '5ee111f6d3dc884052fee12a'
+        ],
+        reviews: ['5ee112cfd3dc884052fee130', '5ee112abd3dc884052fee12e'],
+        ratings: 3.5
+      }
+    ],
+    score: 0.6184504628181458
+  }
+];
+
+function dynamicSort(property, order) {
+  let sort_order = 1;
+  if (order === 'descending') {
+    sort_order = -1;
+  }
+  return function (e1, e2) {
+    const property1 = getProperty(property, e1);
+    const property2 = getProperty(property, e2);
+    if (property1 < property2) {
+      return -1 * sort_order;
+    } else if (property1 > property2) {
+      return 1 * sort_order;
+    } else {
+      return 0 * sort_order;
+    }
+  };
+}
+
+function getProperty(property, elem) {
+  if (property === 'distance') {
+    return elem.shop_docs[0].distance;
+  } else if (property === 'price') {
+    return elem.price;
+  } else if (property === 'ratings') {
+    return elem.shop_docs[0].ratings;
+  } else {
+    return null;
+  }
+}
+
+// const ans = () => {
+//   const array = arr.sort(dynamicSort('ratings', 'descending'));
+// };
+
+// module.exports = ans;
