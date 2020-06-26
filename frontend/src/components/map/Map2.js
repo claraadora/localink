501eeb3124c3c465faa8d7788ab10d4810e950c0
olivia johansen <oliviajjohansen@gmail.com>
2020-06-27 01:00:50 +0800

    Comments

--- frontend/src/components/map/Map2.js
+++ frontend/src/components/map/Map2.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   withGoogleMap,
   withScriptjs,
@@ -6,10 +6,10 @@ import {
   Marker,
   DirectionsRenderer,
   Polyline,
-  InfoWindow,
-} from "react-google-maps";
-import { makeStyles } from "@material-ui/styles";
-import { useSelector } from "react-redux";
+  InfoWindow
+} from 'react-google-maps';
+import { makeStyles } from '@material-ui/styles';
+import { useSelector } from 'react-redux';
 import {
   pink,
   deepPurple,
@@ -18,8 +18,8 @@ import {
   teal,
   lime,
   orange,
-  amber,
-} from "@material-ui/core/colors";
+  amber
+} from '@material-ui/core/colors';
 
 function createKey(location) {
   return location.lat + location.lng;
@@ -32,23 +32,23 @@ function createPolyline(start, i) {
     zIndex: 1,
     tag: {
       index: start,
-      nestedIndex: i,
-    },
+      nestedIndex: i
+    }
   });
 }
-const colors = ["#ff99ff", "#99c2ff", "#ff8080", "#ff8c1a"];
+const colors = ['#ff99ff', '#99c2ff', '#ff8080', '#ff8c1a'];
 const A = { lat: 1.30655, lng: 103.773523 };
 const B = { lat: 1.31655, lng: 103.773523 };
 const C = { lat: 1.32655, lng: 103.773523 };
 const D = { lat: 1.308086, lng: 103.773538 };
 
 function Map() {
-  const productArray = useSelector((state) => state.search.productArray);
-  const loading = useSelector((state) => state.search.loading);
+  const productArray = useSelector(state => state.search.productArray);
+  const loading = useSelector(state => state.search.loading);
   const [searchResult, setSearchResult] = useState(null);
   const [startPoints, setStartPoints] = useState([A, B]);
   const [endPoints, setEndPoints] = useState([B, C]);
-  const [travelMode, setTravelMode] = useState("DRIVING");
+  const [travelMode, setTravelMode] = useState('DRIVING');
   const [routeData, setRouteData] = useState([]);
   const [directions, setDirections] = useState(null);
 
@@ -70,6 +70,7 @@ function Map() {
           origin: startPoints[i],
           destination: endPoints[i],
           travelMode: travelMode,
+          provideRouteAlternatives: true
         },
         (response, status) => {
           if (status === window.google.maps.DirectionsStatus.OK) {
@@ -84,7 +85,7 @@ function Map() {
                 fare: response.routes[j].fare,
                 end_address: response.routes[j].legs[0].end_address,
                 start_address: response.routes[j].legs[0].start_address,
-                routeIndex: j,
+                routeIndex: j
               });
             }
             start++;
@@ -100,7 +101,7 @@ function Map() {
     }
   }, [startPoints, endPoints]);
 
-  const loopThru = (directions) => {
+  const loopThru = directions => {
     for (let i = 0; i < directions.length; i++) {
       for (let j = 0; j < directions[i].length; j++) {
         return <DirectionsRenderer directions={directions[i]} />;
@@ -110,14 +111,14 @@ function Map() {
 
   function createMarker(latLng, i) {
     const marker = new window.google.maps.Marker({
-      position: latLng,
+      position: latLng
       // label: labels[i],
     });
   }
 
   return (
     <GoogleMap defaultZoom={12} defaultCenter={{ lat: 1.3521, lng: 103.8198 }}>
-      {console.log("routeData" + routeData)}
+      {console.log('routeData' + routeData)}
       {searchResult == null
         ? null
         : searchResult.map((product, index) => {
@@ -142,7 +143,7 @@ const MapWrapped = withScriptjs(withGoogleMap(Map));
 
 export const LocalinkMap = () => {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+    <div style={{ width: '100vw', height: '100vh' }}>
       <MapWrapped
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
