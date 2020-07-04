ddecbd04c33a5e4cfeff2c3049e2b28557c8d532
olivia johansen <oliviajjohansen@gmail.com>
2020-07-04 18:54:26 +0800

    Remove client dir in paths

--- backend/server.js
+++ backend/server.js
@@ -119,11 +119,9 @@ io.on('connection', socket => {
 //Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use(express.static('frontend/client/build'));
+  app.use(express.static('frontend/build'));
   app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, 'frontend', 'client', 'build', 'index.html')
-    );
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
