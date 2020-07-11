d0d47b723556881eacb3dea9bbba839edbd68184
olivia johansen <oliviajjohansen@gmail.com>
2020-07-11 21:11:47 +0800

    Format error msg

--- middleware/business/authRequired.js
+++ middleware/business/authRequired.js
@@ -7,6 +7,10 @@ module.exports = async function (req, res, next) {
     req.user = user;
     next();
   } else {
-    res.send('Account not activated, click here to send email');
+    res
+      .status(400)
+      .json({
+        errors: [{ msg: 'Account not activated, click here to send email' }]
+      });
   }
 };
