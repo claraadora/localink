eecc3010e67a0ce8301472995fa166d4530e0a97
Clara Adora <claraadora@gmail.com>
2020-07-18 15:54:21 +0800

    Fix layout of seller navbar

--- frontend/src/components/navbar/SellerNavBar.js
+++ frontend/src/components/navbar/SellerNavBar.js
@@ -1,56 +1,51 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+import { AppBar, Toolbar, Typography, Button, Grid } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-  grow: {
-    flexGrow: 1,
-  },
-  toolbar: {
-    minHeight: 40,
-    alignItems: "flex-start",
-    paddingTop: theme.spacing(0),
-    paddingBottom: theme.spacing(1),
-  },
-  root: {
-    display: "flex",
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-}));
-
-export default function LoggedSellerNavBar() {
-  const classes = useStyles();
-
+export default function SellerNavBar() {
   return (
-    <div className={classes.grow}>
-      <AppBar position="static">
-        <Toolbar className={classes.toolbar}>
-          <Button
-            color="inherit"
-            component={Link}
-            to="/business"
-            backgroundColor="transparent"
-          >
-            <Typography variant="h1">localink for sellers</Typography>
-          </Button>
-          <div className={classes.grow} />
-          <Button color="inherit" component={Link} to="/">
-            shop on localink
-          </Button>
-          <Button color="inherit" component={Link} to="/business/signup">
-            Sign up
-          </Button>
-          <Button color="inherit" component={Link} to="/business/login">
-            Log in
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Grid container direction="row" spacing={2} md={12} alignItems="center">
+          <Grid item md={1} xs={0} />
+          <Grid item container justify="flex-start" md={2} xs={2}>
+            <Button color="inherit" href="/business">
+              <Grid
+                item
+                container
+                direction="row"
+                alignItems="flex-end"
+                spacing={1}
+              >
+                <Grid item>
+                  <Typography variant="h1">localink</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h6">for sellers</Typography>
+                </Grid>
+              </Grid>
+            </Button>
+          </Grid>
+          <Grid item md={5} xs={0} />
+          <Grid item container md={4} xs={8} justify="space-around">
+            <Grid item>
+              <Button color="inherit" component={Link} to="/">
+                Shop on localink
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button color="inherit" component={Link} to="/business/signup">
+                Sign Up
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button color="inherit" component={Link} to="/business/login">
+                Log In
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 }
