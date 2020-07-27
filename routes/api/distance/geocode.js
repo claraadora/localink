a5a8de6e9e8802ff6b40c5a71161872a92e17415
Clara Adora <claraadora@gmail.com>
2020-07-27 16:06:48 +0800

    Modify geocode function to get country too

--- routes/api/distance/geocode.js
+++ routes/api/distance/geocode.js
@@ -1,27 +1,40 @@
-const got = require('got');
+const got = require("got");
 
-const frontURI = 'https://maps.googleapis.com/maps/api/geocode/json?';
-const APIkey = 'key=' + process.env.GOOGLE_MAPS_API_KEY;
+const frontURI = "https://maps.googleapis.com/maps/api/geocode/json?";
+const APIkey = "key=" + process.env.GOOGLE_MAPS_API_KEY;
 
 module.exports = async function geocode(addr) {
   try {
-    const address = 'address=' + convertSpaceToPlus(addr);
-    const URI = frontURI + address + '&' + APIkey;
+    const address = "address=" + convertSpaceToPlus(addr);
+    const URI = frontURI + address + "&" + APIkey;
     const response = await got(URI);
     const results = JSON.parse(response.body).results;
+
+    // GET LAT LNG
     const latLng = results[0].geometry.location;
-    return latLng;
+    let country = "";
+
+    const arr = results[0].address_components;
+    if (latLng !== "") {
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i].types[0] === "country") {
+          country = country + arr[i].long_name;
+          break;
+        }
+      }
+    }
+    return { latLng: latLng, country: country };
   } catch (error) {
     console.log(error);
   }
 };
 
 function convertSpaceToPlus(string) {
-  let stringArr = string.split(' ');
-  let finalString = '';
+  let stringArr = string.split(" ");
+  let finalString = "";
   for (let i = 0; i < stringArr.length - 1; i++) {
-    if (stringArr[i].charAt(0) != '#') {
-      finalString = finalString + stringArr[i] + '+';
+    if (stringArr[i].charAt(0) != "#") {
+      finalString = finalString + stringArr[i] + "+";
     }
   }
   finalString += stringArr[stringArr.length - 1];
