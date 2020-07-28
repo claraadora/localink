703fe20c46fbb0e8b4362150d124eee2cbe66763
olivia johansen <oliviajjohansen@gmail.com>
2020-07-28 17:58:18 +0800

    duplicate

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,61 +1,59 @@
-import React, { useEffect } from "react";
-import { ThemeProvider } from "@livechat/ui-kit";
-import { LocalinkChatList } from "../components/chat/ChatList";
-import { LocalinkMessageList } from "../components/chat/MessageList";
-import { useDispatch, useSelector } from "react-redux";
-import { Grid, makeStyles, CircularProgress } from "@material-ui/core";
-import { themeChat } from "../themeChat";
-import io from "socket.io-client";
-import { getChatList, afterPostMessage } from "../actions/chatActions";
-import profileConstants from "../constants/profileConstants";
+import React, { useEffect } from 'react';
+import { ThemeProvider } from '@livechat/ui-kit';
+import { LocalinkChatList } from '../components/chat/ChatList';
+import { LocalinkMessageList } from '../components/chat/MessageList';
+import { useDispatch, useSelector } from 'react-redux';
+import { Grid, makeStyles, CircularProgress } from '@material-ui/core';
+import { themeChat } from '../themeChat';
+import io from 'socket.io-client';
+import { getChatList, afterPostMessage } from '../actions/chatActions';
+import profileConstants from '../constants/profileConstants';
 
 const socket = io();
 
 const useStyles = makeStyles({
   root: {
-    display: "flex",
-    flexDirection: "column",
-    height: "90vh",
+    display: 'flex',
+    flexDirection: 'column',
+    height: '90vh'
   },
   chatList: {
-    flex: "30%",
-    display: "flex",
-    minHeight: "100px",
-    flexDirection: "column",
-    overflow: "hidden",
-    borderRight: "solid 1px #f6f6f6",
+    flex: '30%',
+    display: 'flex',
+    minHeight: '100px',
+    flexDirection: 'column',
+    overflow: 'hidden',
+    borderRight: 'solid 1px #f6f6f6'
   },
   messageList: {
-    flex: "70%",
-    display: "flex",
-    flexDirection: "column",
-    overflow: "hidden",
-    background: "#fff",
-  },
+    flex: '70%',
+    display: 'flex',
+    flexDirection: 'column',
+    overflow: 'hidden',
+    background: '#fff'
+  }
 });
 
-export const ChatPage = (props) => {
+export const ChatPage = props => {
   const dispatch = useDispatch();
-  const isShopper = useSelector((state) => state.page.isShopper);
-  const user = useSelector((state) => state.auth.user);
-  const profile = useSelector((state) => state.profile.profile);
-  const loading = useSelector((state) => state.chat.loading);
-  const chat = useSelector((state) => state.chat);
+  const isShopper = useSelector(state => state.page.isShopper);
+  const user = useSelector(state => state.auth.user);
+  const profile = useSelector(state => state.profile.profile);
+  const loading = useSelector(state => state.chat.loading);
+  const chat = useSelector(state => state.chat);
   const classes = useStyles();
 
   useEffect(() => {
-    socket.on("Output Chat Message", (messageFromBackEnd) => {
+    socket.on('Output Chat Message', messageFromBackEnd => {
       let myId = isShopper ? user._id : profile._id;
-      let selectSenderId = isShopper ? "shop" : "shopper";
-      let selectReceiverId = isShopper ? "shopper" : "shop";
+      let selectSenderId = isShopper ? 'shop' : 'shopper';
+      let selectReceiverId = isShopper ? 'shopper' : 'shop';
 
-      console.log("myId" + myId);
+      console.log('myId' + myId);
 
-      if (messageFromBackEnd[selectSenderId] !== myId) {
-        console.log("yay, message is not from me. Is it for me tho?");
+      if (messageFromBackEnd.message.userId !== myId) {
         console.log(messageFromBackEnd);
         if (messageFromBackEnd[selectReceiverId] === myId) {
-          console.log("its mine!");
           dispatch(afterPostMessage({ messageFromBackEnd, isShopper }));
         }
       }
@@ -71,9 +69,9 @@ export const ChatPage = (props) => {
     return (
       <Grid
         container
-        justify="center"
-        alignItems="center"
-        style={{ height: "100%", width: "100%" }}
+        justify='center'
+        alignItems='center'
+        style={{ height: '100%', width: '100%' }}
       >
         <Grid item>
           <CircularProgress />
@@ -83,22 +81,22 @@ export const ChatPage = (props) => {
   }
 
   const styleShopper = {
-    height: "92vh",
-    paddingTop: "8vh",
+    height: '92vh',
+    paddingTop: '8vh'
   };
 
   const styleSeller = {
-    height: "92vh",
-    width: "calc(100vw - 200px)",
-    marginLeft: "200px",
-    paddingTop: "8vh",
+    height: '92vh',
+    width: 'calc(100vw - 200px)',
+    marginLeft: '200px',
+    paddingTop: '8vh'
   };
 
   return (
     <ThemeProvider theme={themeChat}>
       <Grid
         container
-        direction="row"
+        direction='row'
         style={isShopper ? styleShopper : styleSeller}
       >
         <Grid item md={3}>
@@ -108,8 +106,8 @@ export const ChatPage = (props) => {
           item
           container
           md={9}
-          style={{ height: "92vh" }}
-          direction="column"
+          style={{ height: '92vh' }}
+          direction='column'
         >
           <LocalinkMessageList socket={socket} />
         </Grid>
